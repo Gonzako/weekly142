87649bf9c7c01f59261fce1204385af57c16781c
Gonzako <al341522@uji.es>
2020-03-28 17:02:28 +0100

    assdf

---
+++ Assets/Scripts/2DGameScripts/Scripts/AI/aiKnockBacker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aiKnockBacker : MonoBehaviour
+{
+    public Vector2 velTorqueWhenThrownHit = new Vector2(4, 20);
+    public float timeToWait = 2f;
+    Rigidbody2D rb;
+    eventDisabler disabler;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        disabler = GetComponent<eventDisabler>();
+    }
+
+
+    public void turnIntoDinamic(GameObject caller)
+    {
+        rb.isKinematic = false;
+        disabler.OnEventRaised(timeToWait);
+        Vector2 direction = this.transform.position - caller.transform.position;
+        direction = direction.normalized;
+        rb.velocity = direction * velTorqueWhenThrownHit.x;
+        rb.angularVelocity = Random.value * 10 % 2 == 0 ?  velTorqueWhenThrownHit.y : - velTorqueWhenThrownHit.y;
+        StartCoroutine(back2Dinamic(timeToWait));
+    }
+
+    IEnumerator back2Dinamic(float time)
+    {
+        yield return new WaitForSeconds(time);
+        rb.isKinematic = true;
+    }
+}
---
+++ Assets/simpleMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float maxVel = 2f;
+
+    private Vector3 inputVect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        inputVect.x = Input.GetAxis("Horizontal");
+        inputVect.z = Input.GetAxis("Vertical");
+        if(inputVect.magnitude > 0)
+        {
+            transform.rotation = Quaternion.AngleAxis(Vector3.Angle(transform.position, transform.position + inputVect), Vector3.up);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = inputVect.normalized * maxVel;
+    }
+}
