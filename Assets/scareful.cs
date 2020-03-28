c42015c0be5c479c2b716afa3919aad03b294ac2
Gonzako <al341522@uji.es>
2020-03-28 14:39:39 +0100

    added KennyNL furniture assets

---
+++ Assets/scareful.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scareful : MonoBehaviour
+{
+
+    private List<scareful> otherSacarefuls;
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player")) 
+        {
+
+        }
+        if (collision.gameObject.CompareTag("Other"))
+        {
+            otherSacarefuls.Add(collision.GetComponent<scareful>());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Other"))
+        {
+            otherSacarefuls.Remove(other.gameObject.GetComponent<scareful>());
+        }
+    }
+
+}
